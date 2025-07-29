a4d0648aab28b6a80edbfc9a87b74920c4ea3844
Dharani Limmada <105279279+dharanilimmada@users.noreply.github.com>
2025-07-29 22:26:21 +0530

    Add files via upload

---
+++ arrayevenandodd.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class EvenOddCounter {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of elements: ");
+        int size = scanner.nextInt();
+
+        int[] numbers = new int[size];
+        System.out.println("Enter " + size + " numbers:");
+        for (int i = 0; i < size; i++) {
+            numbers[i] = scanner.nextInt();
+        }
+
+        int evenCount = 0;
+        int oddCount = 0;
+
+        for (int num : numbers) {
+            if (num % 2 == 0) {
+                evenCount++;
+            } else {
+                oddCount++;
+            }
+        }
+
+        System.out.println("Total Even Numbers: " + evenCount);
+        System.out.println("Total Odd Numbers: " + oddCount);
+        scanner.close();
+    }
+}
